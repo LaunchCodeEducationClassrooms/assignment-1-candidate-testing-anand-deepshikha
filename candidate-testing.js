7bd2c04a39e0af60f8b82f1af05baf9b726ac558
deepshikha80 <deepshikha.anand80@gmail.com>
2021-02-18 20:22:44 +0000

    questions array

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,7 +1,7 @@
 const input = require('readline-sync');
 count=0;
 // TODO 2: modify your quiz app to ask 5 questions //
-let questions=["Who was the first American woman in space? ", "True or false: 5000 meters == 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let questions=["Who was the first American woman in space? ", "True or false: 5000 meters == 5 kilometers? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers=["Sally Ride","true","40","Trajectory","3"];
 
 // TODO 1.1a: Define candidateName // 
@@ -25,8 +25,7 @@ function askQuestion() {
   let i;
   for(i=0;i<questions.length;i++)
   {
-    console.log(`${i+1}. ${questions[i]}`);
-  candidateAnswers[i]=input.question("Please Enter your answer:");
+    candidateAnswers[i]=input.question(`${questions[i]}`);
   }
   //console.log(question);
   //candidateAnswer=input.question("Please Enter your answer:");
